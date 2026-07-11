b6e7b89f55f408f5fda1490a6c1676380c25dc1b
Revaldi-lang <revaldifirdaus28@gmail.com>
2026-07-11 12:10:37 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ export default function RootLayout({
   return (
     <html lang="id" className="h-full antialiased">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" rel="stylesheet" />
       </head>
